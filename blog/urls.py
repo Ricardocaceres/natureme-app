45869ebcaba1cdfb84e0a0200b9482358c20a860
Ricardo <ricardo@natureme.com.au>
2016-06-26 14:30:20 +1000

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
+	
+	#Include url for single page post, called 'post detail' in post_list.html
+	
+	#?P<pk> means django will take everything to be placed there and transfer it to a view called 'pk'
+	#\d means it can only be a digit, + means there needs to be one or more digits there
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
+#include get_object_or_404
+from django.shortcuts import render, get_object_or_404
 #include timezone import used to publish blog
 from django.utils import timezone
 #Include models that have been written in models.py
 #Dot means current directory/application
 from .models import Post
 
+#Here is where we will define our views. This means what html files we want to show
+
 #define function to post blogposts
 def post_list(request):
 	#publish blog posts sorted by 'published date'
 	posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	#return function render, paremeter request and a template file 'blog/post_list.html'
-	#{} add things for template to use
+	#return function render, brr(request, 'blog/post_list.html', {'posts':posts})
 	return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+	post=get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post':post})
+
